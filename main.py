86ff184adcc2c780ce6d0d29a1eb6321979ccab5
enzoagw <enzogonzaga@aluno.fapce.edu.br>
2022-10-13 19:31:50 -0300

    Geração de matrizes

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import functions
+import numpy as np
+import random
 
+matrizes = []
+n = int(input('Digite o número de matrizes a serem geradas: '))
+for i in range(0,n):
+    dimensao  = random.randint(2, 5)
+    a = np.random.randint(5, size=(dimensao, dimensao))
+    matrizes.append(a)
 if __name__ == '__main__':
     functions.determinant([[1, 3, 7, 1], [4.5, 10, -10, 5], [32, 9, -1, -4], [0, 3, -8, 2]])
