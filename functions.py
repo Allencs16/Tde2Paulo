c14b8d467321843f3f336cac5516f4b05749b73b
enzoagw <enzogonzaga@aluno.fapce.edu.br>
2022-10-14 15:03:33 -0300

    Soma, subtração e multi

--- functions.py
+++ functions.py
@@ -3,3 +3,25 @@ import numpy as np
 def determinant(matrix):
     array = np.array(matrix)
     print(np.linalg.det(array))
+
+def matrixSum(matrix):
+    array = matrix
+    while i <= len(array):
+        if i + 1 != None: 
+            if array[i].shape[0] == array[i+1].shape[0]:
+                print(array[i]+array[i+1])
+def matrixSubt(matrix):
+    array = matrix
+    while i <= len(array):
+        if i + 1 != None: 
+            if array[i].shape[0] == array[i+1].shape[0]:
+                print(array[i] - array[i+1])
+def matrixmult(matrix):
+    array = matrix
+    while i <= len(array):
+        if i + 1 != None: 
+            if array[i].shape[1] == array[i+1].shape[0]:
+                array[i].dot(array[i+1])
+                print(array[i].dot(array[i+1]))
+            else:
+                print('Impossivel multiplicar')
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import functions
 import numpy as np
+import pandas as pd
 import random
 
 matrizes = []
@@ -8,5 +9,6 @@ for i in range(0,n):
     dimensao  = random.randint(2, 5)
     a = np.random.randint(5, size=(dimensao, dimensao))
     matrizes.append(a)
+
 if __name__ == '__main__':
     functions.determinant([[1, 3, 7, 1], [4.5, 10, -10, 5], [32, 9, -1, -4], [0, 3, -8, 2]])
