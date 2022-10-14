472b87167af366671b54cc07b8107d1135c59f4b
Breno AllenCS <brenoallencs@gmail.com>
2022-10-14 00:10:39 -0300

    Sum matrix

--- functions.py
+++ functions.py
@@ -2,4 +2,16 @@ import numpy as np
 
 def determinant(matrix):
     array = np.array(matrix)
-    print(np.linalg.det(array))
+    shape = array.shape
+    if(shape[0] != shape[1]):
+        print('Você deve enviar um array quadrado')
+        return
+    else:
+        print(np.linalg.det(array))
+
+def somaMatrix(matrix1):
+    array1 = np.array(matrix1)
+    print(array1[0])
+    print(array1[1])
+    print(array1[0] + array1[1])
+
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import functions
 
 if __name__ == '__main__':
-    functions.determinant([[1, 3, 7, 1], [4.5, 10, -10, 5], [32, 9, -1, -4], [0, 3, -8, 2]])
+    #functions.determinant([[1, 3, 7, 1], [4.5, 10, -10, 5], [32, 9, -1, -4]])
+    functions.somaMatrix([[[1, 2], [0, 2]], [[1, 1], [0, 2]]])
